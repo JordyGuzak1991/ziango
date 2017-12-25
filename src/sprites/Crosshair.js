5f63084a8e0df44cec7534521d34af434208a9c2
Jordy Guzak <j.guzak@student.fontys.nl>
2017-12-25 20:09:24 +0100

    scale crosshair

--- src/sprites/Crosshair.js
+++ src/sprites/Crosshair.js
@@ -5,12 +5,11 @@ export default class extends Phaser.Sprite {
     constructor ({game, x, y, asset}) {
         super(game, x, y, asset)
         this.anchor.setTo(0.5)
-        this.scale.setTo(config.scaleRatio, config.scaleRatio)
-
+        this.scale.setTo(config.scaleRatio(), config.scaleRatio())
         game.physics.p2.enable(this)
-        game.add.existing(this)
         this.body.static = true
         this.body.setCircle(10)
         this.body.data.shapes[0].sensor = true
+        game.add.existing(this)
     }
 }
